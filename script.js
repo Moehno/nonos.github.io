062f11206407c38bf548071c8a2c67d2f5dc650d
Moehno <91035191+Moehno@users.noreply.github.com>
2026-01-21 22:14:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,12 @@ creationForm.addEventListener('submit', (event) => {
 });
 
 function createBoard(selectedRows, selectedCols) {
-    const oldInnerGameWrapper = document.getElementById("innerGameWrapper");
-    const newInnerGameWrapper = document.createElement("div");
+    const innerGameWrapper = document.getElementById("innerGameWrapper");
+    const tempInnerGameWrapper = document.createElement("div");
+    const rowHintsInDocument = document.getElementById("gameRowHints");
+    const tempRowHintsInDocument = document.createElement("div");
+    const colHintsInDocument = document.getElementById("gameColHints");
+    const tempColHintsInDocument = document.createElement("div");
     maxRows = selectedRows;
     maxCols = selectedCols;
 
@@ -25,6 +29,11 @@ function createBoard(selectedRows, selectedCols) {
     for (let k = 0; k < selectedRows; k++) {
         const newRow = document.createElement("div");
         newRow.classList.add("gameRow");
+
+        const newRowHintRow = document.createElement("div");
+        newRowHintRow.classList.add("gameRowHintRow");
+        tempRowHintsInDocument.appendChild(newRowHintRow);
+
         for (let l = 0; l < selectedCols; l++) {
             const newBox = document.createElement("div");
             newBox.classList.add("gameBox");
@@ -33,18 +42,19 @@ function createBoard(selectedRows, selectedCols) {
             newRow.appendChild(newBox);
         }
 
-        newInnerGameWrapper.appendChild(newRow);
+        tempInnerGameWrapper.appendChild(newRow);
     }
 
-    oldInnerGameWrapper.innerHTML = newInnerGameWrapper.innerHTML;
+    innerGameWrapper.innerHTML = tempInnerGameWrapper.innerHTML;
+    rowHintsInDocument.innerHTML = tempRowHintsInDocument.innerHTML;
 
-    // assign DOM board to a variable as a 2D Array
+    // assign DOM board to a variable as a 2D array
     boardInDocument = Array.from(document.getElementById("innerGameWrapper").querySelectorAll(".gameRow"));
     boardInDocument = boardInDocument.map(gameRow =>
         Array.from(gameRow.querySelectorAll(".gameBox"))
     );
 
-    // create 2D intern board for faster calculations
+    // create intern board as 2D array for faster calculations
     for (let i = 0; i < selectedRows; i++) {
         board[i] = [];
         rowHints[i] = [];
@@ -53,15 +63,7 @@ function createBoard(selectedRows, selectedCols) {
             colHints[j] = [];
         }
     }
-
-
-    console.log(oldInnerGameWrapper);
-    console.log(rowHints);
-    console.log(colHints);
 }
-
-
-
 
 document.getElementById("outerGameWrapper").addEventListener("click", (element) => {
     const targetBox = element.target.closest(".gameBox");
@@ -70,25 +72,25 @@ document.getElementById("outerGameWrapper").addEventListener("click", (element) 
     const targetValue = board[targetRow][targetCol];
 
     if (targetValue === 0) {
-        console.log("Empty space found");
+        // console.log("Empty space found");
         board[targetRow][targetCol] = 1;
         boardInDocument[targetRow][targetCol].classList.add("checked");
-        createHints(targetRow, targetCol);
+        getHint(targetRow, targetCol);
     } else if (targetValue === 1) {
-        console.log("Filled Space found");
+        // console.log("Filled Space found");
         board[targetRow][targetCol] = 0;
         boardInDocument[targetRow][targetCol].classList.remove("checked");
-        createHints(targetRow, targetCol);
+        getHint(targetRow, targetCol);
     } else {
-        console.log("Error");
+        // console.log("Error");
     }
 });
 
-function createHints(row, col) {
+function getHint(row, col) {
     const currentRowHints = [];
     const currentColHints = [];
 
-    //create row hint
+    // get row hint
     let currentHint = 0;
     for (let i = 0; i < maxCols; i++) {
         const currentValue = board[row][i];
@@ -106,11 +108,11 @@ function createHints(row, col) {
                 currentRowHints.push(currentHint);
             }
         } else {
-            console.log("Error at rowHint creation")
+            // console.log("Error at rowHint creation");
         }
     }
 
-    // create col hint
+    // get col hint
     currentHint = 0;
     for (let j = 0; j < maxRows; j++) {
         const currentValue = board[j][col];
@@ -128,15 +130,55 @@ function createHints(row, col) {
                 currentColHints.push(currentHint);
             }
         } else {
-            console.log("Error at colHint creation")
+            // console.log("Error at colHint creation");
         }
     }
 
     rowHints[row] = currentRowHints;
     colHints[col] = currentColHints;
-    console.log(rowHints, colHints);
+
+    renderHint(row, col)
 }
 
+function renderHint(row, col) {
+    const rowHintRowsInDocument = Array.from(document.getElementById("gameRowHints").querySelectorAll(".gameRowHintRow"));
+    const rowHintAmount = getLargestSubarrayLength(rowHints);
+
+    // render empty boxes
+    rowHints.forEach((element, index) => {
+        const tempRowHintRowInDocument = document.createElement("div");
+        for (i = 0; i < rowHintAmount; i++) {
+            const newHintBox = document.createElement("div");
+            newHintBox.classList.add("gameRowHintBox");
+            tempRowHintRowInDocument.appendChild(newHintBox);
+        }
+        rowHintRowsInDocument[index].innerHTML = tempRowHintRowInDocument.innerHTML;
+    });
+
+    // enter numbers into each box
+    rowHints.forEach((element, i) => {
+        // console.log(element)
+        element.forEach((e, j) => {
+            console.log(rowHintRowsInDocument[i].children[j]);
+            rowHintRowsInDocument[i].children[j].textContent = e;
+            console.log(e)
+        })
+    });
+
+    // render col hint
+}
+
+function getLargestSubarrayLength(array) {
+    maxLength = 0;
+
+    array.forEach(element => {
+        if (element.length > maxLength) {
+            maxLength = element.length;
+        }
+    });
+
+    return maxLength;
+}
 
 function checkAllHints() {
 
